69cf19c3b986c85b3a0048669ccccfeb418033c3
Sebastian L <sebastian.luque.pl@gmail.com>
2026-02-04 10:42:05 -0500

    update build

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,20 +1,20 @@
 import tailwindcss from '@tailwindcss/vite'
-import { nitroV2Plugin } from '@tanstack/nitro-v2-vite-plugin'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
+import { nitro } from 'nitro/vite'
 import { defineConfig } from 'vite'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 
 const config = defineConfig({
   plugins: [
-    nitroV2Plugin(),
+    tanstackStart(),
+    nitro(),
+    viteReact(),
     // this is the plugin that enables path aliases
     viteTsConfigPaths({
       projects: ['./tsconfig.json']
     }),
-    tailwindcss(),
-    tanstackStart(),
-    viteReact()
+    tailwindcss()
   ],
   optimizeDeps: {
     exclude: ['@mapbox']
